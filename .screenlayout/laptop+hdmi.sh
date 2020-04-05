6adaafb5eb3931f34c8bcec974100038f8a7e9b2
Alexander Heldt <me@alexanderheldt.se>
2020-04-05 20:06:16 +0200

    Update laptop+htmi layout
    Set auto resolution on hdmi display

--- .screenlayout/laptop+hdmi.sh
+++ .screenlayout/laptop+hdmi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --scale 1.5x1.5 --mode 2560x1440 --pos 2560x0 --rotate normal
+xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --scale 1.5x1.5 --auto --pos 2560x0 --rotate normal
