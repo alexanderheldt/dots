9b48324ca8c5ee19452f3dc2b6e568364f02cff3
Alexander Heldt <me@alexanderheldt.se>
2021-03-08 20:22:48 +0100

    Update screenlayout that uses laptop and external monitor
    Set `DISPLAY=:0` to be able to call the script as root from an udev
    rule. This is needed as root does not know which displays the current
    user has

--- .screenlayout/laptop+hdmi.sh
+++ .screenlayout/laptop+hdmi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --scale 1.5x1.5 --auto --pos 2560x0 --rotate normal
+DISPLAY=:0 /usr/bin/xrandr --output eDP-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-1 --scale 1.5x1.5 --auto --pos 2560x0 --rotate normal
