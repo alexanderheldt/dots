04d20bcbb0230e4ad8fcd0754e6be2a3dd86f711
Alexander Heldt <me@alexanderheldt.se>
2021-03-13 19:10:45 +0100

    emacs: Don't use C-g as a way to return to normal mode in evil mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -112,7 +112,6 @@
   (setq evil-want-C-i-jump nil)
   :config
   (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
 
   ;; Use visual line motions even outside of visual-line-mode buffers
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
