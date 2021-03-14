589874a74641b05f84285c15d8dd0905dc8f685d
Alexander Heldt <me@alexanderheldt.se>
2021-03-14 18:24:12 +0100

    emacs: Add hydra

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -125,6 +125,8 @@
   :config
   (evil-collection-init))
 
+(use-package hydra)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
