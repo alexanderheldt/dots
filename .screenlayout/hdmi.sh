f8a1e65f499eec865add0c765b239fff95675d23
Alexander Heldt <me@alexanderheldt.se>
2021-07-01 13:31:09 +0200

    screen layout: Set scaling when using only HDMI

--- .screenlayout/hdmi.sh
+++ .screenlayout/hdmi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DISPLAY=:0 /usr/bin/xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-2 --off --output HDMI-2 --off
+DISPLAY=:0 /usr/bin/xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 2560x1440 --scale 1.5x1.5 --pos 0x0 --rotate normal --output DP-2 --off --output HDMI-2 --off
