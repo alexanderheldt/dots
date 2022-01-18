7615cf52232eeb4e654e5ed0b88c671eac0728da
Alexander Heldt <me@alexanderheldt.se>
2022-01-18 10:28:50 +0100

    screenlayout: rearrange monitors

--- .screenlayout/laptop+hdmi.sh
+++ .screenlayout/laptop+hdmi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DISPLAY=:0 /usr/bin/xrandr --output eDP-1 --primary --mode 2560x1440 --pos 3840x0 --rotate normal --output HDMI-1 --scale 1.5x1.5 --auto --pos 0x0 --rotate normal
+DISPLAY=:0 /usr/bin/xrandr --output eDP-1 --primary --mode 2560x1440 --pos 640x2160 --rotate normal --output HDMI-1 --scale 1.5x1.5 --auto --pos 0x0 --rotate normal
